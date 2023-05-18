c28088a65b54bbd1e8dbee2cf0d1c2f001c32132
Ser4ld <S1097973@studenti.univpm.it>
2023-05-18 18:11:39 +0200

    DatabaseSeeder.php updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,56 +13,79 @@ class DatabaseSeeder extends Seeder {
 
     public function run() {
 
-        DB::table('category')->insert([
-            ['catId' => 1, 'name' => 'Computer', 'parId' => 0, 'desc' => 'Desktop, Laptop, Netbook'],
-            ['catId' => 2, 'name' => 'Periferiche', 'parId' => 0, 'desc' => 'Hard Disk, Tastiere, Mouse'],
-            ['catId' => 3, 'name' => 'Desktop', 'parId' => 1, 'desc' => 'Descrizione dei Prodotti Desktop'],
-            ['catId' => 4, 'name' => 'Laptop', 'parId' => 1, 'desc' => 'Descrizione dei Prodotti Laptop'],
-            ['catId' => 5, 'name' => 'NetBook', 'parId' => 1, 'desc' => 'Descrizione dei Prodotti Netbook'],
-            ['catId' => 6, 'name' => 'HardDisk', 'parId' => 2, 'desc' => 'Descrizione dei Dischi Rigidi'],
+        DB::table('faq')->insert([
+            ["domanda" => "Posso avere accesso ai coupon senza registrarmi?", "risposta" => "- No. Per accedere ai vari coupon bisogna essere registrati al nostro sito."],
+            ["domanda" => "I coupon hanno una scadenza?", "risposta" => "- Sì, i coupon hanno una data di scadenza"],
+            ["domanda" => "Ci sono limiti di utilizzo per i coupon?", "risposta" => "- Sì. Per ogni cliente registrato al sito si può erogare al massimo un coupon per ogni promozione."],
+            ["domanda" => "Posso avere accesso ai coupon senza registrarmi?", "risposta" => "- No. Per accedere ai vari coupon bisogna essere registrati al nostro sito."],
+            ["domanda" => "I coupon hanno una scadenza?", "risposta" => "- Sì, i coupon hanno una data di scadenza."]
+            ]);
+
+        DB::table('utente')->insert([
+            [ 'username' => 'useruser', 'password' => 'nYvpLFCp',
+                'nome' => 'Utente', 'cognome' => 'Registrato', 'genere' => 'M', 'eta' => 25, 'email' => 'user@example.com', 'telefono' => 1234567890,
+                'via' => 'Via Utente Registrato', 'numero_civico' => 1, 'citta' => 'Città Utente Registrato', 'livello' => 1,
+                'remember_token' => Str::random(10), 'created_at' => now(), 'updated_at' => now()],
+            [ 'username' => 'staffstaff',
+                'password' => 'nYvpLFCp',
+                'nome' => 'Membro',
+                'cognome' => 'Staff',
+                'genere' => 'M',
+                'eta' => 30,
+                'email' => 'staff@example.com',
+                'telefono' => 9876543210,
+                'via' => 'Via Membro Staff',
+                'numero_civico' => 2,
+                'citta' => 'Città Membro Staff',
+                'livello' => 2,
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [ 'username' => 'adminadmin',
+                'password' => 'nYvpLFCp',
+                'nome' => 'Amministratore',
+                'cognome' => 'Admin',
+                'genere' => 'M',
+                'eta' => 35,
+                'email' => 'admin@example.com',
+                'telefono' => 5432167890,
+                'via' => 'Via Amministratore',
+                'numero_civico' => 3,
+                'citta' => 'Città Amministratore',
+                'livello' => 3,
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+
         ]);
 
-        DB::table('product')->insert([
-            ['name' => 'NetBook Modello2', 'catId' => 5,
-                'descShort' => 'Caratteristiche NetBook2', 'descLong' => self::DESCPROD,
-                'price' => 219.34, 'discountPerc' => 12, 'discounted' => 0, 'image' => ''],
-            ['name' => 'HardDisk Modello2', 'catId' => 6,
-                'descShort' => 'Caratteristiche HardDisk2', 'descLong' => self::DESCPROD,
-                'price' => 86.37, 'discountPerc' => 15, 'discounted' => 1, 'image' => 'Italy.gif'],
-            ['name' => 'Desktop Modello1', 'catId' => 3,
-                'descShort' => 'Caratteristiche Desktop1', 'descLong' => self::DESCPROD,
-                'price' => 1230.49, 'discountPerc' => 25, 'discounted' => 1, 'image' => 'Brazil.gif'],
-            ['name' => 'Laptop Modello1', 'catId' => 4,
-                'descShort' => 'Caratteristiche Laptop1', 'descLong' => self::DESCPROD,
-                'price' => 455.37, 'discountPerc' => 17, 'discounted' => 1, 'image' => ''],
-            ['name' => 'Laptop Modello2', 'catId' => 4,
-                'descShort' => 'Caratteristiche Laptop1', 'descLong' => self::DESCPROD,
-                'price' => 1889.67, 'discountPerc' => 15, 'discounted' => 1, 'image' => 'Argentina.gif'],
-            ['name' => 'Netbook Modello3', 'catId' => 5,
-                'descShort' => 'Caratteristiche NetBook3', 'descLong' => self::DESCPROD,
-                'price' => 259.99, 'discountPerc' => 17, 'discounted' => 0, 'image' => 'Red Apple.gif'],
-            ['name' => 'Laptop Modello3', 'catId' => 4,
-                'descShort' => 'Caratteristiche Laptop3', 'descLong' => self::DESCPROD,
-                'price' => 998.99, 'discountPerc' => 23, 'discounted' => 1, 'image' => 'UK.gif'],
-            ['name' => 'HardDisk Modello1', 'catId' => 6,
-                'descShort' => 'Caratteristiche HardDisk1', 'descLong' => self::DESCPROD,
-                'price' => 88.93, 'discountPerc' => 5, 'discounted' => 0, 'image' => 'USA.gif'],
-            ['name' => 'HardDisk Modello4', 'catId' => 6,
-                'descShort' => 'Caratteristiche HardDisk4', 'descLong' => self::DESCPROD,
-                'price' => 78.66, 'discountPerc' => 7, 'discounted' => 01, 'image' => 'Ukraine.gif']
+        DB::table('azienda')->insert([
+            ["partita_iva"=>"00011122233", "nome" => "mediaworld", "localita" => "Ancona", "tipologia" =>"elettronica", "descrizione" => "negozio di elettronica e tecnologia", "logo"=>"prova.jpg", "ragione_sociale" => "S.p.a"],
+            ["partita_iva"=>"00011122234", "nome" => "euronics", "localita" => "Pescara", "tipologia" =>"elettronica", "descrizione" => "negozio di elettronica e tecnologia", "logo"=>"prova.jpg", "ragione_sociale" => "S.p.a"],
+            ["partita_iva"=>"00011122235", "nome" => "H&M", "localita" => "Ancona", "tipologia" =>"abbigliamento", "descrizione" => "negozio di abbigliamento", "logo"=>"prova.jpg", "ragione_sociale" => "S.p.a"],
+
         ]);
 
-        DB::table('users')->insert([
-            ['name' => 'Alex', 'surname' => 'Verdi', 'email' => 'alex@verdi.it', 'username' => 'alexalex',
-                'password' => Hash::make('alexalex'), 'role' => 'user','created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")],
-            ['name' => 'Marco', 'surname' => 'Bianchi', 'email' => 'marco@bianchi.it', 'username' => 'useruser',
-                'password' => Hash::make('useruser'), 'role' => 'user', 'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")],
-            ['name' => 'Mario', 'surname' => 'Rossi', 'email' => 'mario@rossi.it', 'username' => 'adminadmin',
-                'password' => Hash::make('adminadmin'), 'role' => 'admin', 'created_at' => date("Y-m-d H:i:s"),
-                'updated_at' => date("Y-m-d H:i:s")]
+        DB::table('offerta')->insert([
+        ["data_scadenza"=> "2023/06/20", "luogo_fruizione"=> "mediaworld", "modalita_fruizione"=>"online", "percentuale_sconto"=> 20, "prezzo_pieno" => 300, "oggetto_offerta"=>"AirPods 2 Pro", "azienda"=>"00011122233", "staff"=>"staffstaff", "categoria" => "elettronica"],
+        ["data_scadenza"=> "2023/07/23", "luogo_fruizione"=> "euronics", "modalita_fruizione"=>"negozio fisico", "percentuale_sconto"=> 10, "prezzo_pieno" => 800, "oggetto_offerta"=>"laptop ASUS", "azienda"=>"00011122234", "staff"=>"staffstaff", "categoria" => "elettronica"],
+        ["data_scadenza"=> "2023/08/21", "luogo_fruizione"=> "H&M", "modalita_fruizione"=>"negozio fisico", "percentuale_sconto"=> 15, "prezzo_pieno" => 50, "oggetto_offerta"=>"Maglioncino Cropp", "azienda"=>"00011122235", "staff"=>"staffstaff", "categoria" => "abbigliamento"]
+
         ]);
+
+
+
+        DB::table('gestione')->insert([
+            ["staff"=> "staffstaff", "azienda" => "00011122233"],
+            ["staff"=> "staffstaff", "azienda" => "00011122234"],
+            ["staff"=> "staffstaff", "azienda" => "00011122235"]
+        ]);
+
+
+
+
     }
 
 }
