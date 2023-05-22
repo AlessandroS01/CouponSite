415d8d6a71e00f43eafc968520f047b48fc9525c
AlessandroS01 <seghini53@gmail.com>
2023-05-22 10:33:35 +0200

    SISTEMATA LA PARTE DELL'AUTENTICAZIONE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,42 +8,52 @@
 
     <div class="container-login">
 
-        <h1>Login: Utilizza questa form per autenticarti al sito</h1>
+        <h1>Login</h1>
 
         <div class="container-auth">
             {{ Form::open(array('route' => 'login', 'class' => 'contact-form')) }}
 
-             <div  class="wrap-input">
-                 <p> Se non hai già un account <a  href="{{ route('register') }}">registrati</a></p>
-             </div>
-
 
              <div  class="container-dati-login">
-                {{ Form::label('username', 'Nome Utente', ['class' => 'label-input']) }}
+                {{ Form::label('username', 'Username', ['class' => 'label-input']) }}
                 {{ Form::text('username', '', ['class' => 'input','id' => 'username']) }}
+
+            </div>
+            <div class="container-dati-login_errors">
+
                 @if ($errors->first('username'))
-                <ul class="errors">
-                    @foreach ($errors->get('username') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
+                    <ul class="errors">
+                        @foreach ($errors->get('username') as $message)
+                            <li>{{ $message }}</li>
+                        @endforeach
+                    </ul>
                 @endif
+
             </div>
 
              <div  class="container-dati-login">
                 {{ Form::label('password', 'Password', ['class' => 'label-input']) }}
                 {{ Form::password('password', ['class' => 'input', 'id' => 'password']) }}
+
+            </div>
+            <div class="container-dati-login_errors">
+
                 @if ($errors->first('password'))
-                <ul class="errors">
-                    @foreach ($errors->get('password') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
+                    <ul class="errors">
+                        @foreach ($errors->get('password') as $message)
+                            <li>{{ $message }}</li>
+                        @endforeach
+                    </ul>
                 @endif
+
+            </div>
+
+            <div  class="wrap-input">
+                <p> Se non hai già un account <a  href="{{ route('register') }}">registrati</a></p>
             </div>
 
             <div class="container-autenticazione_button">
-                {{ Form::submit('Login', ['class' => 'form-btn1']) }}
+                {{ Form::submit('Login', ['class' => 'submit-button']) }}
             </div>
 
             {{ Form::close() }}
