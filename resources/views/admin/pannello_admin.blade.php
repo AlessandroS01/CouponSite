d08caaca9947efba701cfc0e6d4be289bbd328f5
AlessandroS01 <seghini53@gmail.com>
2023-05-24 14:41:16 +0200

    CREATO PANNELLO DELL'ADMIN

--- resources/views/admin/pannello_admin.blade.php
+++ resources/views/admin/pannello_admin.blade.php
@@ -4,4 +4,42 @@
 
 @section('content')
 
+    <div class="container-gestione_titolo">
+        <h1> Gestione admin</h1>
+    </div>
+
+    <div class="container-pannello_ad">
+
+        <div class="container-pannello_admin">
+
+
+            <div class="container-gestione_aziende">
+                <h2> Gestione Aziende</h2>
+
+                <h4><a href="#">+ Creazione azienda</a></h4>
+                <h4><a href="#">+ Modifica azienda</a></h4>
+                <h4><a href="#">+ Elimina azienda</a></h4>
+            </div>
+
+
+            <div class="container-gestione_staff">
+                <h2> Gestione Staff</h2>
+
+                <h4><a href="#">+ Aggiunta staff</a></h4>
+                <h4><a href="#">+ Modifica staff</a></h4>
+                <h4><a href="#">+ Eliminazione staff</a></h4>
+            </div>
+
+            <div class="container-gestione_admin">
+                <h2> Gestione Generale</h2>
+
+                <h4><a href="#">+ Modifica faq</a></h4>
+                <h4><a href="#">+ Visualizzazione statistiche</a></h4>
+                <h4><a href="#">+ Eliminazione cliente</a></h4>
+            </div>
+        </div>
+
+    </div>
+
+
 @endsection
