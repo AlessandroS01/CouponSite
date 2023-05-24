41df132aaddfc2dbe9fe8311c5e2e7e9cab3178a
Michelangelo <mikimarconi2001@gmail.com>
2023-05-24 11:52:01 +0200

    modifiche profilo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\CatalogoOfferte;
 use App\Models\ProfileUser;
 use App\Models\Resources\Product;
 use App\Http\Requests\NewProductRequest;
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
@@ -37,5 +38,23 @@ function showProfilo(){
         ->with('coupons', $coupons);
 
 }
+
+function modificaProfilo(Request $request){
+    $request->validate([
+        'username' => ['required', 'string', 'min:8', 'max:50', 'unique:users'],
+        'password' => ['required', 'max:50', 'confirmed', Rules\Password::defaults()],
+        'nome' => ['required', 'string', 'max:50'],
+        'cognome' => ['required', 'string', 'max:50'],
+        'genere' => ['required', 'string', 'max:1'],
+        'eta' => ['required', 'int'],
+        'email' => ['required', 'string', 'email', 'max:50', 'unique:users'],
+        'telefono' => ['required', 'numeric', 'digits_between:10,20'],
+        'via' => ['required', 'string', 'max:100'],
+        'numero_civico' => ['required', 'int'],
+        'citta' => ['required', 'string', 'max:50'],
+    ]);
+}
+
+
 
 }
