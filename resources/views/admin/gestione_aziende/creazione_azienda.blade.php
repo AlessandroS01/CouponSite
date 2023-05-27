4de80312edcabc6760fa96cb90531365cf6127fb
Michelangelo <mikimarconi2001@gmail.com>
2023-05-27 12:23:19 +0200

    creazione azienda stile

--- resources/views/admin/gestione_aziende/creazione_azienda.blade.php
+++ resources/views/admin/gestione_aziende/creazione_azienda.blade.php
@@ -9,15 +9,15 @@
         <h1> Aggiunta Azienda</h1>
     </div>
 
-    <div class="container-creazione_staff">
-        <div class="form-creazione_staff">
-            <div class="container-form-creazione_staff">
+    <div class="container-form">
+        <div class="form">
+            <div class="container-form-gestione">
                 <div>
                     {{ Form::open(array('route' => 'aggiunta azienda', 'class' => 'contact-form', 'method' => 'POST')) }}
 
-                    <div  class="container-dati-creazione_staff">
+                    <div  class="container-dati_form">
                         {{ Form::label('nome', 'Nome', ['class' => 'label-input']) }}
-                        {{ Form::text('nome', '', ['class' => 'input', 'id' => 'nome']) }}
+                        {{ Form::text('nome', '', ['class' => 'input']) }}
                     </div>
                     @if ($errors->first('nome'))
                         <ul class="errors">
@@ -28,9 +28,23 @@
                     @endif
 
 
-                    <div  class="container-dati-creazione_staff">
+                    <div class="container-dati_form">
+                        {{ Form::label('località', 'Località', ['class' => 'label-input']) }}
+                        {{ Form::text('località', '', ['class' => 'input']) }}
+
+                    </div>
+                    @if ($errors->first('località'))
+                        <ul class="errors">
+                            @foreach ($errors->get('località') as $message)
+                                <li>{{ $message }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
+
+                    <div  class="container-dati_form">
                         {{ Form::label('partita_iva', 'Partita Iva', ['class' => 'label-input']) }}
-                        {{ Form::text('partita_iva', '', ['class' => 'input', 'id' => 'cognome' ]) }}
+                        {{ Form::text('partita_iva', '', ['class' => 'input']) }}
 
                     </div>
                     @if ($errors->first('partita_iva'))
@@ -41,22 +55,10 @@
                         </ul>
                     @endif
 
-                    <div  class="container-dati-creazione_staff">
-                        {{ Form::label('descrizione', 'Descrizione', ['class' => 'label-input']) }}
-                        {{ Form::text('descrizione', '', ['class' => 'input', 'id' => 'cognome' ]) }}
 
-                    </div>
-                    @if ($errors->first('descrizione'))
-                        <ul class="errors">
-                            @foreach ($errors->get('descrizione') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-
-                    <div  class="container-dati-creazione_staff">
+                    <div  class="container-dati_form">
                         {{ Form::label('email', 'Email', ['class' => 'label-input']) }}
-                        {{ Form::text('email', '', ['class' => 'input','id' => 'email']) }}
+                        {{ Form::text('email', '', ['class' => 'input']) }}
 
                     </div>
                     @if ($errors->first('email'))
@@ -70,23 +72,9 @@
 
 
                 <div>
-
-                    <div class="container-dati-creazione_staff">
-                        {{ Form::label('località', 'Località', ['class' => 'label-input']) }}
-                        {{ Form::text('località', '', ['class' => 'input', 'id' => 'eta']) }}
-
-                    </div>
-                    @if ($errors->first('località'))
-                        <ul class="errors">
-                            @foreach ($errors->get('località') as $message)
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-
-                    <div class="container-dati-creazione_staff">
+                    <div class="container-dati_form">
                         {{ Form::label('telefono', 'Telefono', ['class' => 'label-input']) }}
-                        {{ Form::text('telefono', '', ['class' => 'input', 'id' => 'telefono']) }}
+                        {{ Form::text('telefono', '', ['class' => 'input']) }}
                     </div>
                     @if ($errors->first('telefono'))
                         <ul class="errors">
@@ -97,9 +85,9 @@
                     @endif
 
 
-                    <div  class="container-dati-creazione_staff">
+                    <div  class="container-dati_form">
                         {{ Form::label('logo', 'Logo', ['class' => 'label-input']) }}
-                        {{ Form::text('logo', '', ['class' => 'input', 'id' => 'cognome' ]) }}
+                        {{ Form::text('logo', '', ['class' => 'input']) }}
 
                     </div>
                     @if ($errors->first('logo'))
@@ -110,9 +98,9 @@
                         </ul>
                     @endif
 
-                    <div class="container-dati-creazione_staff">
+                    <div class="container-dati_form">
                         {{ Form::label('Tipologia', 'Tipologia', ['class' => 'label-input']) }}
-                        {{ Form::text('Tipologia', '', ['class' => 'input', 'id' => 'numero_civico']) }}
+                        {{ Form::text('Tipologia', '', ['class' => 'input']) }}
 
                     </div>
                     @if ($errors->first('Tipologia'))
@@ -124,9 +112,9 @@
                     @endif
 
 
-                    <div class="container-dati-creazione_staff">
+                    <div class="container-dati_form">
                         {{ Form::label('ragione_sociale', 'Ragione Sociale', ['class' => 'label-input']) }}
-                        {{ Form::text('ragione_sociale', '', ['class' => 'input', 'id' => 'numero_civico']) }}
+                        {{ Form::text('ragione_sociale', '', ['class' => 'input']) }}
 
                     </div>
                     @if ($errors->first('ragione_sociale'))
@@ -138,9 +126,27 @@
                     @endif
 
                 </div>
+
+
             </div>
 
-            <div class="container-creazione_staff_button">
+            <div class="form_descrizione">
+                <div  class="container-dati_form_descrizione">
+                    {{ Form::label('descrizione', 'Descrizione', ['class' => 'label-input']) }}
+                    {{ Form::textarea('descrizione', '', ['class' => 'input_descrizione']) }}
+
+                </div>
+                @if ($errors->first('descrizione'))
+                    <ul class="errors">
+                        @foreach ($errors->get('descrizione') as $message)
+                            <li>{{ $message }}</li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div>
+
+
+            <div class="container-form_button">
                 {{ Form::submit('Aggiungi', ['class' => 'submit-button']) }}
             </div>
 
