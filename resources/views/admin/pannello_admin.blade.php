c29c91cda78005450cdfc095f39857fd17ffc6f8
MarcoSpina01 <marco.spina01@icloud.com>
2023-05-27 11:01:42 +0200

    Modifica pannello admin

--- resources/views/admin/pannello_admin.blade.php
+++ resources/views/admin/pannello_admin.blade.php
@@ -9,36 +9,32 @@
     </div>
 
     <div class="container-pannello_ad">
-
-        <div class="container-pannello_admin">
-
-
-            <div class="container-gestione_aziende">
-                <h2> Gestione Aziende</h2>
-
-                <h4><a href="#">+ Creazione azienda</a></h4>
-                <h4><a href="#">+ Modifica azienda</a></h4>
-                <h4><a href="#">+ Elimina azienda</a></h4>
-            </div>
-
-
-            <div class="container-gestione_staff">
-                <h2> Gestione Staff</h2>
-
-                <h4><a href="{{ route('aggiunta staff') }}">+ Aggiunta staff</a></h4>
-                <h4><a href="#">+ Modifica staff</a></h4>
-                <h4><a href="#">+ Eliminazione staff</a></h4>
-            </div>
-
-            <div class="container-gestione_admin">
-                <h2> Gestione Generale</h2>
-
-                <h4><a href="#">+ Modifica FAQs</a></h4>
-                <h4><a href="#">+ Visualizzazione statistiche</a></h4>
-                <h4><a href="#">+ Eliminazione cliente</a></h4>
-            </div>
+        <div class="container-gestione_aziende grid-item">
+            <h2>Gestione Aziende</h2>
+            <h4><a href="#">+ Creazione azienda</a></h4>
+            <h4><a href="#">+ Modifica azienda</a></h4>
+            <h4><a href="#">+ Elimina azienda</a></h4>
         </div>
 
+        <div class="container-gestione_staff grid-item">
+            <h2>Gestione Staff</h2>
+            <h4><a href="{{ route('aggiunta staff') }}">+ Aggiunta staff</a></h4>
+            <h4><a href="#">+ Modifica staff</a></h4>
+            <h4><a href="#">+ Eliminazione staff</a></h4>
+        </div>
+
+        <div class="container-gestione_admin grid-item">
+            <h2>Gestione Generale</h2>
+            <h4><a href="#">+ Visualizzazione statistiche</a></h4>
+            <h4><a href="#">+ Eliminazione cliente</a></h4>
+        </div>
+
+        <div class="container-gestione_admin grid-item">
+            <h2>Gestione FAQ</h2>
+            <h4><a href="#">+ Aggiungi FAQ</a></h4>
+            <h4><a href="#">+ Modifica FAQ</a></h4>
+            <h4><a href="#">+ Elimina FAQ</a></h4>
+        </div>
     </div>
 
 
